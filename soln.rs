fb53ba2c337efa5345e90bc6c8ceef46024d20c3
Bart Massey <bart.massey@gmail.com>
2022-12-13 17:21:06 -0800

    advent of code 2022 day 09

---
+++ soln.rs
@@ -0,0 +1,15 @@
+// This program is licensed under the "MIT License".
+// Please see the file LICENSE in this distribution
+// for license terms.
+
+//! Advent of Code Day 9.  
+//! Ben and Bart Massey 2022
+
+use aoc::*;
+
+fn main() {
+    match get_part() {
+        Part1 => todo!(),
+        Part2 => todo!(),
+    }
+}
